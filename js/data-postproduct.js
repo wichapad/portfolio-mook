e555ef627d2662fc1d338d6d65b608a286ebc939
Wichapad Muenjumrad <wichapadmuenjumrad@Wichapads-MacBook-Pro.local>
2025-08-02 19:48:46 +0700

    create footer and creative product

---
+++ js/data-postproduct.js
@@ -0,0 +1,11 @@
+const postcarousel = [
+    "../assets/img/post-product/IG1.png",
+    "../assets/img/post-product/IG2.png",
+    "../assets/img/post-product/IG3.png",
+    "../assets/img/post-product/IG4.png",
+    "../assets/img/post-product/IG5.png",
+    "../assets/img/post-product/IG6.png",
+    
+];
+
+export default postcarousel;
